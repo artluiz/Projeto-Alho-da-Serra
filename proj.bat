e476858698b6a60e18872ed01798c46c1f39d73b
artluiz <luizarthursm@gmail.com>
2024-01-15 14:01:54 -0300

    Jquery e Bootstrap offline

--- proj.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd C:\Users\kirit\Documents\ProjetoAlhodaSerra\
-call .\.venv\Scripts\activate.bat
-python manage.py runserver
--- setup/settings.py
+++ setup/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "crispy_forms",
     "crispy_bootstrap5",
     "todos.apps",
+    'bootstrap5',
 ]
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
